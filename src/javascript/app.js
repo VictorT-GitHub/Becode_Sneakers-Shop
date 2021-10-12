07ad1c7060737f6922c30cdeb2f64a6854d60a10
Victor Tardif <victor.tardif1@gmail.com>
2021-10-12 16:01:07 +0200

    fin du javascript youpii

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,6 +1,8 @@
-// ---------------------------------
-//        HAMBURGER MENU NAV
-// ---------------------------------
+import { addToCartFunction, updateTotalPrice } from "./functions";
+
+// -------------------------------------------
+//         HAMBURGER MENU NAVIGATION
+// -------------------------------------------
 const burgerMenuDiv = document.querySelector(".burger");
 const burgerBTNS = document.querySelectorAll(".toggleJsBtn");
 
@@ -11,24 +13,87 @@ for (let btn of burgerBTNS) {
 // ---------------------------------
 //          SHOPPING CART
 // ---------------------------------
-// creation element in shopping list section (for add to cart btn)
-// const shoppingList = document.querySelector('.shoppingList')
+// SHOPPING CART TOGGLE BTN
+const shoppingCartToggle = document.querySelector(".shoppingCartToggle");
+const shoppingCart = document.querySelector(".shoppingCart");
 
-// const cartItemImg = document.createElement('div');
-// cartItemImg.classList.add('');
+shoppingCartToggle.addEventListener("click", () =>
+  shoppingCart.classList.toggle("open")
+);
 
-// const cartItemInfo = document.createElement('div');
-//   const cartItemTitle = document.createElement('h5');
-//     cartItemTitle.innerHTML = ;
-//   const cartItemPriceQuantity = document.createElement('div');
-//     const cartItemPrice = document.createElement('span');
-//       cartItemPrice.innerHTML = ;
-//     let cartItemQuantity = document.createElement('span');
-//       cartItemPrice.innerHTML = ;
-//     let cartItemTOTALPrice = document.createElement('span');
-//       cartItemPrice.innerHTML = ;
+// CHECKOUT BTN (boutton acheter)
+const checkoutBtn = document.querySelector(".checkoutBtn");
 
-// const cartItemRemoveBtn = document.createElement('button');
-// cartItemRemoveBtn.classList.add('');
+checkoutBtn.addEventListener("click", () => {
+  if (document.querySelector(".shoppingList").hasChildNodes()) {
+    alert(
+      "Achat validé. Vous allez être redirigé vers le site internet de votre banque pour confirmer le paiement."
+    );
+  }
 
-// function display shopping cart list -> toggle .open class css on shopping list section
+  for (let elem of document.querySelectorAll(".cartItemSection")) {
+    elem.remove();
+  }
+
+  shoppingCart.classList.toggle("open");
+});
+
+// QUANTITY MODIFICATION BUTTONS
+const quantityNumber = document.querySelector(".quantityNumber");
+const moinsBtn = document.querySelector(".moinsBTN");
+const plusBtn = document.querySelector(".plusBTN");
+
+let QUANTITY = 0;
+
+moinsBtn.addEventListener("click", () => {
+  if (QUANTITY > 0) {
+    QUANTITY--;
+    quantityNumber.innerHTML = QUANTITY;
+  }
+});
+
+plusBtn.addEventListener("click", () => {
+  QUANTITY++;
+  quantityNumber.innerHTML = QUANTITY;
+});
+
+// ADD TO CART BTN
+const addToCartBtn = document.querySelector(".addToCart");
+
+addToCartBtn.addEventListener("click", () => {
+  updateTotalPrice(QUANTITY);
+  addToCartFunction(QUANTITY);
+  shoppingCart.classList.add("open");
+});
+
+// -------------------------------------------
+//          GALLERIE IMAGE BUTTONS
+// -------------------------------------------
+const prevBtn = document.querySelector(".previous");
+const nextBtn = document.querySelector(".next");
+const imgSection = document.querySelector("figure");
+
+const imgArray = [
+  "../images/image-product-1.jpg",
+  "../images/image-product-2.jpg",
+  "../images/image-product-3.jpg",
+  "../images/image-product-4.jpg",
+];
+
+let i = 0;
+
+prevBtn.addEventListener("click", () => {
+  i--;
+  if (i === -1) {
+    i = imgArray.length - 1;
+  }
+  imgSection.style.backgroundImage = `url("${imgArray[i]}")`;
+});
+
+nextBtn.addEventListener("click", () => {
+  i++;
+  if (i === imgArray.length) {
+    i = 0;
+  }
+  imgSection.style.backgroundImage = `url("${imgArray[i]}")`;
+});
